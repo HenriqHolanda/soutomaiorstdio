f5d342577dca80af744a0f027ad7ac314634e221
HenriqHolanda <129341211+HenriqHolanda@users.noreply.github.com>
2024-05-06 20:24:35 -0300

    só uma besteirinha

---
+++ soutomaiorstdio/components/menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from 'next/image';
+
+
+const MenuC = () => {
+    return (
+
+        <h1>HelloWorld!!!adsdasdasdsa</h1>
+
+    )
+}
+
+export default MenuC
---
+++ soutomaiorstdio/pages/home/home.js
@@ -0,0 +1,17 @@
+import MenuC from "../../components/menu"
+
+export default function homepage(){
+
+    return(
+        
+        <div>
+            
+        HelloWorld!
+        <MenuC/>
+        
+        </div>
+
+        
+    )
+
+}
